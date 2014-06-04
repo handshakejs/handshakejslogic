094e653f775bd389f8d8c9e2288ba1558fd9222e
scottmotte <scott@scottmotte.com>
2014-06-04 00:56:33 -0700

    IdentitiesCreate

--- handshakejslogic.go
+++ handshakejslogic.go
@@ -1,23 +1,27 @@
 package handshakejslogic
 
 import (
+	"bytes"
 	"errors"
 	"github.com/dchest/uniuri"
 	"github.com/garyburd/redigo/redis"
 	"github.com/scottmotte/redisurlparser"
 	"log"
+	"math/rand"
+	"strconv"
 	"strings"
+	"time"
+)
+
+const (
+	BASE_10             = "10"
+	AUTHCODE_LIFE_IN_MS = "120000"
+	AUTHCODE_LENGTH     = "4"
 )
 
 var (
 	conn redis.Conn
 )
-
-type App struct {
-	AppName string
-	Email   string
-	Salt    string
-}
 
 type LogicError struct {
 	Code    string
@@ -66,26 +70,73 @@ func AppsCreate(app map[string]interface{}) (map[string]interface{}, *LogicError
 	}
 
 	key := "apps/" + app["app_name"].(string)
-	err := checkIfAppExists(key)
+	err := validateAppDoesNotExist(key)
 	if err != nil {
 		logic_error := &LogicError{"not_unique", "app_name", "app_name must be unique"}
 		return app, logic_error
 	}
 	err = addAppToApps(conn, app["app_name"].(string))
 	if err != nil {
-		logic_error := &LogicError{"unkown", "", err.Error()}
+		logic_error := &LogicError{"unknown", "", err.Error()}
 		return nil, logic_error
 	}
-	err = addAppToKey(conn, key, app)
+	err = saveApp(conn, key, app)
 	if err != nil {
-		logic_error := &LogicError{"unkown", "", err.Error()}
+		logic_error := &LogicError{"unknown", "", err.Error()}
 		return nil, logic_error
 	}
 
 	return app, nil
 }
 
-func addAppToKey(conn redis.Conn, key string, app map[string]interface{}) error {
+func IdentitiesCreate(identity map[string]interface{}) (map[string]interface{}, *LogicError) {
+	var app_name string
+	if str, ok := identity["app_name"].(string); ok {
+		app_name = strings.Replace(str, " ", "", -1)
+	} else {
+		app_name = ""
+	}
+	if app_name == "" {
+		logic_error := &LogicError{"required", "app_name", "app_name cannot be blank"}
+		return identity, logic_error
+	}
+	identity["app_name"] = app_name
+
+	var email string
+	if str, ok := identity["email"].(string); ok {
+		email = strings.Replace(str, " ", "", -1)
+	} else {
+		email = ""
+	}
+	if email == "" {
+		logic_error := &LogicError{"required", "email", "email cannot be blank"}
+		return identity, logic_error
+	}
+	identity["email"] = email
+
+	app_name_key := "apps/" + identity["app_name"].(string)
+	key := app_name_key + "/identities/" + identity["email"].(string)
+
+	err := validateAppExists(app_name_key)
+	if err != nil {
+		logic_error := &LogicError{"not_found", "app_name", "app_name could not be found"}
+		return identity, logic_error
+	}
+	err = addIdentityToIdentities(conn, app_name_key, identity["email"].(string))
+	if err != nil {
+		logic_error := &LogicError{"unknown", "", err.Error()}
+		return identity, logic_error
+	}
+	err = saveIdentity(conn, key, identity)
+	if err != nil {
+		logic_error := &LogicError{"unknown", "", err.Error()}
+		return nil, logic_error
+	}
+
+	return identity, nil
+}
+
+func saveApp(conn redis.Conn, key string, app map[string]interface{}) error {
 	args := []interface{}{key}
 	for k, v := range app {
 		args = append(args, k, v)
@@ -107,7 +158,7 @@ func addAppToApps(conn redis.Conn, app_name string) error {
 	return nil
 }
 
-func checkIfAppExists(key string) error {
+func validateAppDoesNotExist(key string) error {
 	res, err := conn.Do("EXISTS", key)
 	if err != nil {
 		return err
@@ -118,4 +169,80 @@ func checkIfAppExists(key string) error {
 	}
 
 	return nil
+}
+
+func validateAppExists(key string) error {
+	res, err := conn.Do("EXISTS", key)
+	if err != nil {
+		return err
+	}
+	if res.(int64) != 1 {
+		err = errors.New("That app_name does not exist.")
+		return err
+	}
+
+	return nil
+}
+
+func addIdentityToIdentities(conn redis.Conn, app_name_key string, email string) error {
+	_, err := conn.Do("SADD", app_name_key+"/identities", email)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func saveIdentity(conn redis.Conn, key string, identity map[string]interface{}) error {
+	base_10, err := strconv.Atoi(BASE_10)
+	if err != nil {
+		return err
+	}
+
+	authcode_life_in_ms, err := strconv.Atoi(AUTHCODE_LIFE_IN_MS)
+	if err != nil {
+		return err
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	authcode, err := randomAuthCode()
+	if err != nil {
+		return err
+	}
+	identity["authcode"] = authcode
+	unixtime := (time.Now().Unix() * 1000) + int64(authcode_life_in_ms)
+	identity["authcode_expired_at"] = strconv.FormatInt(unixtime, base_10)
+
+	args := []interface{}{key}
+	for k, v := range identity {
+		args = append(args, k, v)
+	}
+	_, err = conn.Do("HMSET", args...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func randomAuthCode() (string, error) {
+	base_10, err := strconv.Atoi(BASE_10)
+	if err != nil {
+		return "", err
+	}
+	authcode_length, err := strconv.Atoi(AUTHCODE_LENGTH)
+	if err != nil {
+		return "", err
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	var buffer bytes.Buffer
+
+	for i := 1; i <= authcode_length; i++ {
+		random_number := int64(rand.Intn(10))
+		number_as_string := strconv.FormatInt(random_number, base_10)
+		buffer.WriteString(number_as_string)
+	}
+
+	return buffer.String(), nil
 }
--- handshakejslogic_test.go
+++ handshakejslogic_test.go
@@ -7,10 +7,11 @@ import (
 )
 
 const (
-	APP_NAME  = "app0"
-	EMAIL     = "app0@mailinator.com"
-	SALT      = "1234"
-	REDIS_URL = "redis://127.0.0.1:11001"
+	APP_NAME       = "app0"
+	EMAIL          = "app0@mailinator.com"
+	IDENTITY_EMAIL = "identity0@mailinator.com"
+	SALT           = "1234"
+	REDIS_URL      = "redis://127.0.0.1:11001"
 )
 
 func tempredisConfig() tempredis.Config {
@@ -86,21 +87,21 @@ func TestAppsCreateCustomBlankSalt(t *testing.T) {
 	})
 }
 
-//func TestAppsCreateBlankAppName(t *testing.T) {
-//	tempredis.Temp(tempredisConfig(), func(err error) {
-//		if err != nil {
-//			panic(err)
-//		}
-//
-//		app := map[string]interface{}{"email": EMAIL, "app_name": ""}
-//
-//		handshakejslogic.Setup(REDIS_URL)
-//		_, logic_error := handshakejslogic.AppsCreate(app)
-//		if logic_error.Code != "required" {
-//			t.Errorf("Error", err)
-//		}
-//	})
-//}
+func TestAppsCreateBlankAppName(t *testing.T) {
+	tempredis.Temp(tempredisConfig(), func(err error) {
+		if err != nil {
+			panic(err)
+		}
+
+		app := map[string]interface{}{"email": EMAIL, "app_name": ""}
+
+		handshakejslogic.Setup(REDIS_URL)
+		_, logic_error := handshakejslogic.AppsCreate(app)
+		if logic_error.Code != "required" {
+			t.Errorf("Error", err)
+		}
+	})
+}
 
 func TestAppsCreateNilAppName(t *testing.T) {
 	tempredis.Temp(tempredisConfig(), func(err error) {
@@ -148,4 +149,125 @@ func TestAppsCreateAppNameWithSpaces(t *testing.T) {
 			t.Errorf("Incorrect combining of app_name " + result["app_name"].(string))
 		}
 	})
+}
+
+func TestIdentitiesCreate(t *testing.T) {
+	tempredis.Temp(tempredisConfig(), func(err error) {
+		if err != nil {
+			panic(err)
+		}
+		setupApp(t)
+
+		identity := map[string]interface{}{"app_name": APP_NAME, "email": IDENTITY_EMAIL}
+		result, logic_error := handshakejslogic.IdentitiesCreate(identity)
+		if logic_error != nil {
+			t.Errorf("Error", logic_error)
+		}
+		if result["authcode_expired_at"] == nil {
+			t.Errorf("Error", result)
+		}
+		if len(result["authcode"].(string)) < 4 {
+			t.Errorf("Error", result)
+		}
+	})
+}
+
+func TestIdentitiesCreateBlankAppName(t *testing.T) {
+	tempredis.Temp(tempredisConfig(), func(err error) {
+		if err != nil {
+			panic(err)
+		}
+		setupApp(t)
+
+		identity := map[string]interface{}{"app_name": "", "email": IDENTITY_EMAIL}
+		_, logic_error := handshakejslogic.IdentitiesCreate(identity)
+		if logic_error.Code != "required" {
+			t.Errorf("Error", logic_error)
+		}
+		if logic_error.Field != "app_name" {
+			t.Errorf("Error", logic_error)
+		}
+	})
+}
+
+func TestIdentitiesCreateNilAppName(t *testing.T) {
+	tempredis.Temp(tempredisConfig(), func(err error) {
+		if err != nil {
+			panic(err)
+		}
+		setupApp(t)
+
+		identity := map[string]interface{}{"email": IDENTITY_EMAIL}
+		_, logic_error := handshakejslogic.IdentitiesCreate(identity)
+		if logic_error.Code != "required" {
+			t.Errorf("Error", logic_error)
+		}
+		if logic_error.Field != "app_name" {
+			t.Errorf("Error", logic_error)
+		}
+	})
+}
+
+func TestIdentitiesCreateNonExistingAppName(t *testing.T) {
+	tempredis.Temp(tempredisConfig(), func(err error) {
+		if err != nil {
+			panic(err)
+		}
+		setupApp(t)
+
+		identity := map[string]interface{}{"app_name": "doesnotexist", "email": IDENTITY_EMAIL}
+		_, logic_error := handshakejslogic.IdentitiesCreate(identity)
+		if logic_error.Code != "not_found" {
+			t.Errorf("Error", logic_error)
+		}
+		if logic_error.Field != "app_name" {
+			t.Errorf("Error", logic_error)
+		}
+	})
+}
+func TestIdentitiesCreateBlankEmail(t *testing.T) {
+	tempredis.Temp(tempredisConfig(), func(err error) {
+		if err != nil {
+			panic(err)
+		}
+		setupApp(t)
+
+		identity := map[string]interface{}{"app_name": APP_NAME, "email": ""}
+		_, logic_error := handshakejslogic.IdentitiesCreate(identity)
+		if logic_error.Code != "required" {
+			t.Errorf("Error", logic_error)
+		}
+		if logic_error.Field != "email" {
+			t.Errorf("Error", logic_error)
+		}
+	})
+}
+
+func TestIdentitiesCreateNilEmail(t *testing.T) {
+	tempredis.Temp(tempredisConfig(), func(err error) {
+		if err != nil {
+			panic(err)
+		}
+		setupApp(t)
+
+		identity := map[string]interface{}{"app_name": APP_NAME}
+		_, logic_error := handshakejslogic.IdentitiesCreate(identity)
+		if logic_error.Code != "required" {
+			t.Errorf("Error", logic_error)
+		}
+		if logic_error.Field != "email" {
+			t.Errorf("Error", logic_error)
+		}
+	})
+}
+
+func setupApp(t *testing.T) {
+	app := map[string]interface{}{"email": EMAIL, "app_name": APP_NAME}
+
+	handshakejslogic.Setup(REDIS_URL)
+	_, logic_error := handshakejslogic.AppsCreate(app)
+	if logic_error != nil {
+		t.Errorf("Error", logic_error)
+	}
+
 }
