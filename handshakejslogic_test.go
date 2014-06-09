10e990ae66f29708c914a0fac42573e639948f82
scottmotte <scott@scottmotte.com>
2014-06-08 20:11:01 -0700

    Revert "Remove for now"
    This reverts commit dd1a4dc6a2a3881a2ebf24ea74499b0cdc78d298.

--- handshakejslogic_test.go
+++ handshakejslogic_test.go
@@ -9,16 +9,17 @@ import (
 )
 
 const (
-	APP_NAME       = "app0"
-	EMAIL          = "app0@mailinator.com"
-	IDENTITY_EMAIL = "identity0@mailinator.com"
-	AUTHCODE       = "5678"
-	SALT           = "1234"
-	REDIS_URL      = "redis://127.0.0.1:11001"
+	APP_NAME           = "app0"
+	EMAIL              = "app0@mailinator.com"
+	IDENTITY_EMAIL     = "identity0@mailinator.com"
+	AUTHCODE           = "5678"
+	SALT               = "1234"
+	REDIS_URL          = "redis://127.0.0.1:11001"
+	DB_ENCRYPTION_SALT = "somesecretsalt"
 )
 
 func defaultOptions() handshakejslogic.Options {
-	value := handshakejslogic.Options{}
+	value := handshakejslogic.Options{DbEncryptionSalt: DB_ENCRYPTION_SALT}
 	return value
 }
 
@@ -495,7 +496,7 @@ func setupAppWithShortAuthcodeLife(t *testing.T) {
 	app := map[string]interface{}{"email": EMAIL, "app_name": APP_NAME}
 
 	// set it negative for test purposes
-	options := handshakejslogic.Options{AuthcodeLifeInMs: -5, AuthcodeLength: 5}
+	options := handshakejslogic.Options{DbEncryptionSalt: DB_ENCRYPTION_SALT, AuthcodeLifeInMs: -5, AuthcodeLength: 5}
 	handshakejslogic.Setup(REDIS_URL, options)
 	_, logic_error := handshakejslogic.AppsCreate(app)
 	if logic_error != nil {
